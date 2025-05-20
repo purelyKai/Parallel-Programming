7b44c29eba53389b909290a1a808a3bd804377e8
Kai Black <kaibblack03+github@gmail.com>
2025-05-19 22:16:55 -0700

    fix use of mpirun to alternative command

--- 7_Fourier_Analysis_Using_MPI/build_and_run.sh
+++ 7_Fourier_Analysis_Using_MPI/build_and_run.sh
@@ -4,13 +4,15 @@
 echo "Processors, Elements, MegaSumsPerSecond" > performance_data.csv
 
 # Compile with MPI and link math library
-mpicxx -o main main.cpp -lm
+mpic++ -o main main.cpp -lm
 
 # Number of processors to test
 PROCESSORS=(1 2 4 8 16 32)
 
 # Test each processor count
 for p in "${PROCESSORS[@]}"; do
+    echo "Running with $p processors..."
+
     # Run MPI program and append the performance data to the CSV file
     mpirun -np $p ./main >> performance_data.csv 2>&1
 done
