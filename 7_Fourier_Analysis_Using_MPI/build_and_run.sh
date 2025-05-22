f001d8f8a6c44ed6dff4113fbbff560e5b641ada
Kai Black <kaibblack03+github@gmail.com>
2025-05-22 15:19:45 -0700

    script

--- 7_Fourier_Analysis_Using_MPI/build_and_run.sh
+++ 7_Fourier_Analysis_Using_MPI/build_and_run.sh
@@ -13,20 +13,20 @@
 module load openmpi
 
 # Output CSV header
-echo "Processors, Elements, MegaSumsPerSecond" > performance_data.csv
+# echo "Processors, Elements, MegaSumsPerSecond" > performance_data.csv
 
 # Compile with MPI and link math library
 mpic++ -o main main.cpp -lm
-mpiexec -mca btl self,tcp -np 4 ./main
-## Number of processors to test
-#PROCESSORS=(1 2 4 8 16 32)
-#
-## Test each processor count
-#for p in "${PROCESSORS[@]}"; do
-#    echo "Running with $p processors..."
-#
-#    # Run MPI program and append the performance data to the CSV file
-#    mpiexec -mca btl self,tcp -np $p ./main >> performance_data.csv 2>&1
-#done
-#
-#echo "All tests completed! Results saved in performance_data.csv"
+
+# Number of processors to test
+PROCESSORS=(1 2 4 8 16 32)
+
+# Test each processor count
+for p in "${PROCESSORS[@]}"; do
+    echo "Running with $p processors..."
+
+    # Run MPI program and append the performance data to the CSV file
+    mpiexec -mca btl self,tcp -np $p ./main # >> performance_data.csv 2>&1
+done
+
+echo "All tests completed!" # Results saved in performance_data.csv"
